16972ecb6ce36b725c93b46be51dc83ba0ece8f9
Thomas Chong <thomaschong0929@gmail.com>
2022-11-16 23:16:20 +0800

    updated db schema

--- database/sql/init.sql
+++ database/sql/init.sql
@@ -1,17 +1,16 @@
 CREATE TABLE user (
-    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
+    username varchar(256) NOT NULL PRIMARY KEY,
+    password varchar(256) NOT NULL,
     create_datetime datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE post (
     id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    user_id int(11) NOT NULL,
-    title varchar(255) NOT NULL,
-    content varchar(255) NOT NULL,
+    username varchar(256) NOT NULL,
+    title varchar(256) NOT NULL,
+    content varchar(256) NOT NULL,
     create_datetime datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (username) REFERENCES user(username)
 );
 
 CREATE USER 'appuser'@'172.%' IDENTIFIED BY 'Pa$$w0rd';
